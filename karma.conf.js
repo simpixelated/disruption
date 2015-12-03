335db1bdab744cf5f2c50501da3031c07e17aa56
Jordan Kohl <kohl.jordan@gmail.com>
2015-12-02 22:29:23 -0800

    code coverage [fixes #57]

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // karma.conf.js
-module.exports = function(config) {
+module.exports = function (config) {
 	config.set({
 		frameworks: ['jspm', 'jasmine'],
 		browsers: ['PhantomJS2'],
@@ -9,6 +9,31 @@ module.exports = function(config) {
 			],
 			serveFiles: [
 				'src/**/!(*.spec).js'
+			]
+		},
+		files: [
+			'node_modules/babel-core/browser-polyfill.js'
+		],
+		preprocessors: {
+			'src/**/!(*.spec).js': ['babel', 'coverage']
+		},
+		reporters: ['progress', 'coverage'],
+		babelPreprocessor: {
+			options: {
+				presets: ['es2015'],
+				sourceMap: 'inline'
+			}
+		},
+		coverageReporter: {
+			instrumenters: {
+				isparta: require('isparta')
+			},
+			instrumenter: {
+				'src/**/*.js': 'isparta'
+			},
+			reporters: [
+				{ type: 'html', dir: 'coverage/' },
+				{ type: 'text-summary' }
 			]
 		}
 	});
